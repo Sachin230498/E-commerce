c2a1cf7616e901c60045865b39dfee9552d8c0e9
Sachin230498 <sachinpathe230498@gmail.com>
2023-12-21 19:49:05 +0530

    user service, auth controller

--- ecom-backend/src/server.js
+++ ecom-backend/src/server.js
@@ -1,4 +1,4 @@
-const { connectDb } = require("./db");
+const { connectDb } = require("./config/db");
 const app = require("./index");
 const PORT = process.env.PORT || 5454;
 
---
+++ ecom-backend/src/services/user.service.js
@@ -0,0 +1,78 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwtProvider = require("../config/jwtProvider");
+
+const creatUser = async (userData) => {
+  try {
+    let { firstName, lastName, email, password } = userData;
+
+    const isUserExist = await User.findOne({ email });
+
+    if (isUserExist) {
+      throw new Error("user already exist with email :", email);
+    }
+    password = await bcrypt.hash(password, 8);
+
+    const user = await User.create({ firstName, lastName, email, password });
+
+    console.log("created user", user);
+
+    return user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const findeUserById = async (userId) => {
+  try {
+    const user = await User.findById(userId).populate("address");
+    if (!user) {
+      throw new Error("No user found with id :", userId);
+    }
+    return user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getUserByEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("No user found with email :", email);
+    }
+    return user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getUserProfileByToken = async (token) => {
+  try {
+    const userId = jwtProvider.getUserIdFromToken(token);
+
+    const user = await findeUserById(userId);
+    if (!user) {
+      throw new Error("user not found with id :", userId);
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getAllUsers = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = {
+  creatUser,
+  findeUserById,
+  getUserByEmail,
+  getUserProfileByToken,
+  getAllUsers,
+};
